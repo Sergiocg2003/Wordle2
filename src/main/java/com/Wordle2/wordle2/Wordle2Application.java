79a7be0435ec17f4662cf832ac8d9a5e952eb023
Sergiocg2003 <scorgon265@g.educaand.es>
2023-03-15 20:25:47 +0100

    Primer commit

---
+++ src/main/java/com/Wordle2/wordle2/Wordle2Application.java
@@ -0,0 +1,13 @@
+package com.Wordle2.wordle2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wordle2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wordle2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wordle2/Wordle2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wordle2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wordle2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
